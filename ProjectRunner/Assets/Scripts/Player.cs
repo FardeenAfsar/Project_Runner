bb657b5f2b7fe59bd938a564cc224aacf78d3f6d
Fardeen Afsar <afsarfardeen@gmail.com>
2020-07-16 04:53:32 -0400

    Character Movement Added

---
+++ ProjectRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetpos;
+    public float yinc, speed, minheight, maxheight;
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetpos, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < maxheight)
+        {
+            targetpos = new Vector2(transform.position.x, transform.position.y + yinc);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > minheight)
+        {
+            targetpos = new Vector2(transform.position.x, transform.position.y - yinc);
+        }
+    }
+}

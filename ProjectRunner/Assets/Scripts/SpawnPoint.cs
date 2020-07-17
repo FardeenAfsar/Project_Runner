be96ca111e36c3c87dfab5b9666360419e1ac651
Fardeen Afsar <afsarfardeen@gmail.com>
2020-07-17 10:17:23 -0400

    Added Spawn Patterns

---
+++ ProjectRunner/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject enemy;
+    void Start()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
---
+++ ProjectRunner/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemypatterns;
+    private float time_spawn;
+    public float spawn_time, inc_time, inc_limit = 0.8f;
+    void Update()
+    {
+        if(time_spawn <= 0)
+        {
+            int rand = Random.Range(0, enemypatterns.Length);
+            Instantiate(enemypatterns[rand], transform.position, Quaternion.identity);
+            time_spawn = spawn_time;
+            if(spawn_time > inc_limit)
+            {
+                spawn_time -= inc_time;
+            }
+        }
+        else
+        {
+            time_spawn -= Time.deltaTime;
+        }
+    }
+}

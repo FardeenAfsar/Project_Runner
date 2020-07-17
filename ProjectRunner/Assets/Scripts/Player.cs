e812d4fe3328890c00e2d381320eb9fec5ec1650
Fardeen Afsar <afsarfardeen@gmail.com>
2020-07-17 08:33:50 -0400

    Added Collision

--- ProjectRunner/Assets/Scripts/Player.cs
+++ ProjectRunner/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private Vector2 targetpos;
     public float yinc, speed, minheight, maxheight;
+    public int health = 3;
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, targetpos, speed * Time.deltaTime);

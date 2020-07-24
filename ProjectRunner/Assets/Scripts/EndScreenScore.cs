5553c0eb2ea09a754ae8a1cf22201768de209bf4
Fardeen Afsar <afsarfardeen@gmail.com>
2020-07-24 14:12:39 -0400

    Score System Added

---
+++ ProjectRunner/Assets/Scripts/EndScreenScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EndScreenScore : MonoBehaviour
+{
+    private TextMeshProUGUI bscore;
+    private TextMeshProUGUI curscore;
+    private void Start()
+    {
+        bscore = GameObject.FindGameObjectWithTag("scoresys").GetComponent<TextMeshProUGUI>();
+        curscore = GameObject.FindGameObjectWithTag("scoresys2").GetComponent<TextMeshProUGUI>();
+        bscore.text = Score.hscore.ToString("0");
+        curscore.text = Score.cscore.ToString("0");
+    }
+}
---
+++ ProjectRunner/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    private float startTime, elapsedTime;
+    public static float cscore, hscore = 0f;
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+    private void Update()
+    {
+        elapsedTime = Time.time - startTime;
+        cscore = elapsedTime * 10;
+        text.text = cscore.ToString("0");
+        if(cscore > hscore) { hscore = cscore; }
+    }
+}
